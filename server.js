9cde723c82341e8c45911cc2fecf066e0407933f
Thomas Kim <thomaskim@Thomass-MacBook-Pro.local>
2018-02-10 21:49:49 -0800

    Working simple express server

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 8080;
-app.use(express.static(path.resolve(__dirname, './src')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({ message: "ajsdklfja" });
+});
+
+app.use("/api", router);
+app.listen(PORT);
+console.log('Magic happens on port ' + PORT);
