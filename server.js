d76a37ce90a32efb51ab6a9344eca757a268f1de
Thomas Kim <thomaskim@Thomass-MacBook-Pro.local>
2018-02-12 15:48:07 -0800

    Changed express static path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require('./routes');
 const PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use("/api", routes);
 app.listen(PORT);
