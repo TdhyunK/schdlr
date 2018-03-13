64ad2b5e11893c65f58e6b153ff4053306126774
Thomas Kim <thomaskim@Thomass-MacBook-Pro.local>
2018-03-13 16:57:41 -0700

    Server express server in static directory

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./routes');
-//const cors = require('cors');
 
 const PORT = process.env.PORT || 8080;
 console.log(PORT);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//app.use(cors);
-//app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use("/api", routes);
 app.listen(PORT);
