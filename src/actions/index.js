9a30134c68b298ae510d5165b1d74011987c71be
Thomas Kim <thomaskim@Thomass-MacBook-Pro.local>
2018-03-13 17:08:27 -0700

    Update ROOT_URL to heroku URL

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 import querystring from 'querystring';
 
-export const NUM_FORMS = "NUM_FORMS"; 
 export const GET_POSTS = "NUM_FORMS";
-// const ROOT_URL = "http://schdlr.herokuapp.com/api";
-const ROOT_URL = "http://localhost:8080/api";
+const ROOT_URL = "http://schdlr.herokuapp.com/api";
+// const ROOT_URL = "http://localhost:8080/api";
 
 /**
  * Action to create a post request to fetch valid classes
